4d7256befd28052aa6e3032876b1c3bdb7582db8
jfigueroapcs <jfigueroaubv@gmail.com>
2025-05-30 17:07:42 -0400

    especificando puerto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import dotenv from 'dotenv';
 
 import node from "@astrojs/node";
+
+// Cargar variables de entorno
+dotenv.config();
+
+const port = process.env.ASTRO_PORT 
+  ? parseInt(process.env.ASTRO_PORT) 
+  : 4321;
 
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  server: {
+    port: port,  // Puerto personalizado
+  },
 
   adapter: node({
     mode: "standalone",
